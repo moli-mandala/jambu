0c3f1ed0409def6afa17ccaa4e3a467af5f7326d
aryamanarora <aryaman.arora2020@gmail.com>
2021-03-23 17:02:58 -0400

    add all of Strand's data, and IPA (#9)

--- data/strand.py
+++ data/strand.py
@@ -3,6 +3,9 @@ from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 import csv
 import re
+from segments.tokenizer import Tokenizer, Profile
+
+t = Tokenizer('strand_profile.txt')
 
 chars = ['p', 'b', 'bAsp', 'f', 'v', 'w', 'm', 'uFrn', 'u', 'o', 'oFrn', 'uTns', 'oTns',
          'cDen', 'zDen', 't', 'd', 'dAsp', 's', 'z', 'l', 'lVls', 'n', 'cRet', 'jRet',
@@ -22,6 +25,35 @@ languages = ['IndoAryan/Pashai/Degan/DeganLanguage',
 
 codes = ['deg', 'Kam', 'Kata', 'Ash', 'Wg', 'Kho', 'Phal']
 
+with open('strand2.csv', 'w') as fout:
+    writer = csv.writer(fout)
+    link = f'http://nuristan.info/IndoAryan/SwatIndus/Bhatera/BhateraLanguage/Lexicon/lex.html'
+    try:
+        with urlopen(Request(link, headers={'User-Agent': 'Mozilla/5.0'})) as resp:
+            soup = BeautifulSoup(resp, 'html.parser')
+            for data in soup.find_all(class_='dic'):
+                word = data.find(class_='l')
+                if word:
+                    print(word)
+                    word = word.find(text=True, recursive=False)
+                    word2 = re.sub(r'ʹ(.)', r'\1ʹ', word)
+                    data = str(data).replace('\n', ' ')
+                    l = re.search(r'<b>]</b>\xa0 (.*?)\.\xa0 (.*?)\.', data)
+                    if not l:
+                        l = re.search(r'</span>[\xa0 ]+(.*?)\.\xa0\xa0([^\.]+)\.', data)
+                    print(l)
+                    if l:
+                        pos = l.group(1).lower()
+                        definition = l.group(2).lower()
+                        turner = re.search(r'T\..(\d+)', data)
+                        if turner:
+                            turner = turner.group(1)
+                            ipa = t(word2, column='IPA').replace(' ', '').replace('#', ' ')
+                            writer.writerow(['bhatr', turner, word, definition, '', ipa, '', 'strand'])
+
+    except HTTPError as e:
+        pass
+
 with open('strand.csv', 'w') as fout:
     writer = csv.writer(fout)
     for i, language in enumerate(languages):
@@ -35,6 +67,7 @@ with open('strand.csv', 'w') as fout:
                         word = data.find(class_='l')
                         if word:
                             word = word.find(text=True, recursive=False)
+                            word2 = re.sub(r'ʹ(.)', r'\1ʹ', word)
                             l = re.search(r'<b>]</b>\xa0 (.*?)\.\xa0 (.*?)\.', str(data))
                             if not l:
                                 l = re.search(r'</span>\xa0 (.*?)\.\xa0 (.*?)\.', str(data))
@@ -44,7 +77,8 @@ with open('strand.csv', 'w') as fout:
                                 turner = re.search(r'T\. (\d+)', str(data))
                                 if turner:
                                     turner = turner.group(1)
-                                    writer.writerow([codes[i], turner, word, definition, '', '', '', 'strand'])
+                                    ipa = t(word2, column='IPA').replace(' ', '').replace('#', ' ')
+                                    writer.writerow([codes[i], turner, word, definition, '', ipa, '', 'strand'])
 
             except HTTPError as e:
                 pass
