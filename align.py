987bb93545b2535972bcca2f9cc4e2327af69b0a
aryamanarora <aryaman.arora2020@gmail.com>
2021-04-25 13:05:29 -0400

    Kvari

--- align.py
+++ align.py
@@ -9,29 +9,30 @@ with open('cldf/forms.csv') as fin:
         if i == 0: continue
         if row[1] not in ['Indo-Aryan', 'S']: continue
         if row[2] not in forms: forms[row[2]] = []
-        forms[row[2]].append([row[1], row[3].strip('-, *')])
+        forms[row[2]].append([row[1], row[6].strip('-, *')])
 
 cts = Counter()
 with open('test.out', 'a') as fout:
     for i in forms:
         if len(forms[i]) == 1: continue
-        forms[i].sort()
         try:
+            forms[i].sort()
             m = lingpy.Multiple([x[1] for x in forms[i]])
             m.prog_align()
             strs = [['#'] + list(x.split()) + ['#'] for x in str(m).split('\n')]
             l = len(strs[0])
-            for i in range(l):
-                for reflex in strs:
-                    if reflex[i] == 'b':
-                        l, r = i - 1, i + 1
-                        while strs[0][l] == '-':
-                            l -= 1
-                        while strs[0][r] == '-':
-                            r += 1
-                        
-                        k = f'{strs[0][l]}_{strs[0][i]}_{strs[0][r]}'.replace(' ', '')
-                        cts[k] += 1
+            for char in range(l):
+                for j, reflex in enumerate(strs):
+                    if forms[i][j] != 'Indo-Aryan':
+                        if 'd͡ʒ' == reflex[char]:
+                            l, r = char - 1, char + 1
+                            while strs[0][l] == '-':
+                                l -= 1
+                            while strs[0][r] == '-':
+                                r += 1
+                            
+                            k = f'{strs[0][l]}_{strs[0][char]}_{strs[0][r]} > {reflex[char]}'.replace(' ', '')
+                            cts[k] += 1
         except:
             continue
 
